e6949f6442a4f5062ef9d8ed10634af29c556969
remmykimani <remmykimani@gmail.com>
2018-06-13 18:03:07 +0300

    added files to repo

--- StudentShuffler/StudentShuffler/StudentShuffler.cpp
+++ StudentShuffler/StudentShuffler/StudentShuffler.cpp
@@ -1,17 +1,44 @@
 #include "stdafx.h"
 #include<iostream>
-#include<string>
-#include<vector>
 #include<fstream>
+#include<string>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-	
+	vector<string>all_students;
+	ifstream ics_a("inputICSA.csv");
+
+	if (!ics_a.is_open())
+	{
+		cout << "Not open";
+	}
+	else
+	{
+		string adm;
+		string name;
+		string gender;
+
+		while (ics_a.good())
+		{
+			/*getline(ics_a, adm, '"');
+			getline(ics_a, name, '"');*/
+
+			getline(ics_a, name, '\n');
+
+			all_students.push_back(name);
 
 
-
-
-
-    return 0;
+			/*cout << "Your Admission Number:" << adm << endl;
+			cout <<"Your Name is :"<< name<<endl;
+			cout <<"Your Gender is :"<< gender<<endl;
+			cout << "______________________________\n";*/
+			cout << all_students.at(all_students.size) << endl;
+		}
+	}
+	system("pause");
+	return 0;
 }
 
